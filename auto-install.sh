b22497afac04da9c0ab444f2ea77c4b4738d82ab
layka <layka@layka-K53SV.(none)>
2013-03-24 12:47:44 +0800

    add GNU global

--- auto-install.sh
+++ auto-install.sh
@@ -35,25 +35,33 @@ cd vim
             --enable-pythoninterp \
             --enable-perlinterp \
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
-make VIMRUNTIMEDIR=/usr/share/vim/vim73
+make VIMRUNTIMEDIR=/usr/share/vim/vim73 -j4
 sudo make install
 
 wget http://llvm.org/releases/3.2/clang+llvm-3.2-x86_64-linux-ubuntu-12.04.tar.gz /tmp/clang.tar.gz
-tar -xvf clang.tar.gz -C ~/ycm_temp/llvm_root_dir/
-CLANG_TAR_NAME="`ls ~/ycm_temp/llvm_root_dir | grep clang`"
+tar -xvf /tmp/clang.tar.gz -C /tmp/llvm_root_dir/
+CLANG_TAR_NAME="`ls /tmp/llvm_root_dir | grep clang`"
 
 sudo apt-get install cmake python-dev build-essential
 
-cd ~
+cd /tmp
 mkdir ycm_build
 cd !$
-cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=~/ycm_temp/llvm_root_dir/"$CLANG_TAR_NAME" . ~/.vim/bundle/YouCompleteMe/cpp
+cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=/tmp/llvm_root_dir/"$CLANG_TAR_NAME" . ~/.vim/bundle/YouCompleteMe/cpp
 cd !$ && make ycm_core
 
 # for id-utils and ack.vim
 sudo apt-get install id-utils ack-grep
 
-# install vim plugins
+# for global
+wget ftp://ftp.gnu.org/pub/gnu/global/global-6.2.8.tar.gz /tmp/global.tar.gz
+tar -xvf /tmp/global.tar.gz -C /tmp
+cd /tmp
+./configure
+make -j4
+sudo make install
+
+# install vim 
 git clone https://github.com/zmc963/vimrc.git "$VIMHOME"
 cd "$VIMHOME"
 git submodule update --init
@@ -67,6 +75,10 @@ cd bundle/command-t/ruby/command-t
 # for youcompleteme
 #cd ~/.vim/bundle/YouCompleteMe
 #./install.sh --clang-completer
-cp ~/ycm_temp/llvm_root_dir/"$CLANG_TAR_NAME"/lib/libclang.so ~/.vim/bundle/YouCompleteMe/python
+cp /tmp/llvm_root_dir/"$CLANG_TAR_NAME"/lib/libclang.so ~/.vim/bundle/YouCompleteMe/python
+
+# for global
+mkdir ~/.vim/bundle/gtags/plugin
+cp /usr/local/share/gtags/gtags.vim !$
 
 echo "vgod's vimrc is installed."
